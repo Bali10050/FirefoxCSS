6616d7dbfe00b0d19565d28088310be4647090e6
Séra Balázs <serabali10050@gmail.com>
2023-12-03 00:24:50 +0100

    Add files via upload

---
+++ user.js
@@ -0,0 +1,3 @@
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("svg.context-properties.content.enabled", true);
+user_pref("layout.css.has-selector.enabled", true);
