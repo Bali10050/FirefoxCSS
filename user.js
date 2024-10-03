8565a09c9e99023ef1caafbf433fd7f19a2726ef
Séra Balázs <serabali10050@gmail.com>
2024-10-03 17:49:23 +0200

    Fix for #16

--- user.js
+++ user.js
@@ -1,3 +1,5 @@
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("svg.context-properties.content.enabled", true);
 user_pref("layout.css.has-selector.enabled", true);
+user_pref("browser.display.background_color", "#eee");
+user_pref("browser.display.background_color.dark", "#222");
