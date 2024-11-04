ce8408a1be788473eb0754a85185482052ee404a
Séra Balázs <serabali10050@gmail.com>
2024-11-04 02:20:31 +0100

    Vertical tabs initial changes

--- user.js
+++ user.js
@@ -3,3 +3,6 @@ user_pref("svg.context-properties.content.enabled", true);
 user_pref("layout.css.has-selector.enabled", true);
 user_pref("browser.display.background_color", "#eee");
 user_pref("browser.display.background_color.dark", "#222");
+user_pref("sidebar.verticalTabs", true);
+user_pref("sidebar.revamp", true);
+user_pref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-list\":[],\"unified-extensions-area\":[],\"nav-bar\":[\"stop-reload-button\",\"back-button\",\"forward-button\",\"save-to-pocket-button\",\"downloads-button\",\"firefox-view-button\",\"new-tab-button\",\"urlbar-container\",\"unified-extensions-button\",\"sidebar-button\",\"alltabs-button\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[],\"vertical-tabs\":[\"tabbrowser-tabs\"],\"PersonalToolbar\":[\"import-button\",\"personal-bookmarks\"]},\"seen\":[\"save-to-pocket-button\",\"developer-button\"],\"dirtyAreaCache\":[\"nav-bar\",\"vertical-tabs\",\"PersonalToolbar\",\"toolbar-menubar\",\"TabsToolbar\"],\"currentVersion\":20,\"newElementCount\":7}");
